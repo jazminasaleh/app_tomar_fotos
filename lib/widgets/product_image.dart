81f5fc8b42b722a27f4babe4e75b6464d0b8bcec
jazmin saleh peña <jazminasaleh@gmail.com>
2022-08-25 17:44:49 -0500

    tomar fotos de galeria

--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class ProductIamge extends StatelessWidget {
@@ -15,17 +17,9 @@ class ProductIamge extends StatelessWidget {
         child: Opacity(
           opacity: 0.8,
           child: ClipRRect(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-            child: this.url == null
-            ? Image(image: AssetImage('assets/no-image.png'), fit: BoxFit.cover,)
-            : FadeInImage(
-              image: NetworkImage(this.url!),
-              placeholder: AssetImage('assets/jar-loading.gif'),
-              //para que la imagen tome todo el espacio del container
-              fit: BoxFit.cover,
-            ),
-          ),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+              child: getImage(url)),
         ),
       ),
     );
@@ -41,4 +35,24 @@ class ProductIamge extends StatelessWidget {
                 blurRadius: 10,
                 offset: Offset(0, 5))
           ]);
+  Widget getImage(String? picture) {
+    if (picture == null)
+      return Image(
+        image: AssetImage('assets/no-image.png'),
+        fit: BoxFit.cover,
+      );
+
+    if (picture.startsWith('http')) {
+      return FadeInImage(
+        image: NetworkImage(this.url!),
+        placeholder: AssetImage('assets/jar-loading.gif'),
+        //para que la imagen tome todo el espacio del container
+        fit: BoxFit.cover,
+      );
+    }
+    return Image.file(
+      File(picture),
+      fit: BoxFit.cover,
+    );
+  }
 }
